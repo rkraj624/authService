38dccf5e42b743350b6ebffadb52b2a6680af37c
RK RAJ <ravi.kishan@salescode.ai>
2025-12-14 12:42:57 +0530

    feat : form bases authentication using session

--- src/main/java/com/security/auth/config/SecurityConfig.java
+++ src/main/java/com/security/auth/config/SecurityConfig.java
@@ -1,21 +1,18 @@
 package com.security.auth.config;
 
-import com.security.auth.model.User;
-import com.security.auth.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -24,27 +21,25 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/login", "/error").permitAll()
                         .anyRequest().authenticated()
                 )
+                .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepository) {
-        return (String username) -> {
-            User user = userRepository.findByEmail(username)
-                    .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
-
-            return org.springframework.security.core.userdetails.User
-                    .withUsername(user.getEmail())
-                    .password(user.getPassword())
-                    .authorities(user.getRoles() == null ? Collections.emptyList() : user.getRoles().stream().map(SimpleGrantedAuthority::new).toList())
-                    .build();
-        };
+    public UserDetailsService userDetailsService(){
+        UserDetails userDetails = User.withUsername("user").password(passwordEncoder().encode("user")).roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(userDetails);
     }
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
